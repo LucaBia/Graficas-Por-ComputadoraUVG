3ce1ae9da2e9a3196b77ecc42ee5496042efffbe
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-09-17 21:11:56 -0600

    DR2-LightAndShadows

--- rayTracer.py
+++ rayTracer.py
@@ -1,37 +1,48 @@
 from gl import Raytracer, color
 from obj import Obj, Texture
-from sphere import Sphere, Material
+from sphere import Sphere, Material, PointLight, AmbientLight
 
 
-snow = Material(diffuse = color(0.91, 0.93, 0.95))
-button = Material(diffuse = color(0, 0, 0))
-nose = Material(diffuse = color(1, 0.35, 0.02))
-eye = Material(diffuse = color(1, 1, 1))
-mouth = Material(diffuse = color(0.46, 0.44, 0.43))
+snow = Material(diffuse = color(0.91, 0.93, 0.95), spec = 96)
+button = Material(diffuse = color(0, 0, 0), spec = 32)
+nose = Material(diffuse = color(1, 0.35, 0.02), spec = 64)
+eye = Material(diffuse = color(1, 1, 1), spec = 64)
+mouth = Material(diffuse = color(0.46, 0.44, 0.43), spec = 16)
 
 
 rayTracer = Raytracer(500, 700)
 
+rayTracer.pointLight = PointLight(position = (-0.5, 1, 0), intensity = 1)
+rayTracer.ambientLight = AmbientLight(strength = 0.1)
 
-rayTracer.scene.append(Sphere((0, 1.2,  -4), 0.6, snow))
-rayTracer.scene.append(Sphere((0, 0.2, -4), 0.8, snow))
+rayTracer.scene.append(Sphere((0, 1.4,  -4), 0.6, snow))
+rayTracer.scene.append(Sphere((0, 0.3, -4), 0.8, snow))
 rayTracer.scene.append(Sphere((0, -1, -4), 1, snow))
 
-rayTracer.scene.append(Sphere((0, 0.2,  -3), 0.1, button))
-rayTracer.scene.append(Sphere((0, -0.2, -3), 0.1, button))
-rayTracer.scene.append(Sphere((0, -0.7, -3), 0.13, button))
+# rayTracer.pointLight = PointLight(position = (-1, 0.3, 0), intensity = 1)
+# rayTracer.ambientLight = AmbientLight(strength = 0.1)
 
-rayTracer.scene.append(Sphere((0, 0.9,  -3), 0.1, nose))
+rayTracer.scene.append(Sphere((0, 0.3,  -3), 0.1, button))
+rayTracer.scene.append(Sphere((0, -0.1, -3), 0.1, button))
+rayTracer.scene.append(Sphere((0, -0.6, -3), 0.13, button))
 
-rayTracer.scene.append(Sphere((-0.13, 1.1,  -3), 0.08, eye))
-rayTracer.scene.append(Sphere((0.13, 1.1,  -3), 0.08, eye))
-rayTracer.scene.append(Sphere((-0.11, 0.92,  -2.5), 0.03, button))
-rayTracer.scene.append(Sphere((0.11, 0.92,  -2.5), 0.03, button))
+# rayTracer.pointLight = PointLight(position = (-2, 2, 0), intensity = 1)
+# rayTracer.ambientLight = AmbientLight(strength = 0.1)
 
-rayTracer.scene.append( Sphere((-0.08, 0.7,  -3), 0.04, mouth))
-rayTracer.scene.append( Sphere((0.08, 0.7,  -3), 0.04, mouth))
-rayTracer.scene.append( Sphere((-0.20, 0.77,  -3), 0.04, mouth))
-rayTracer.scene.append( Sphere((0.20, 0.77,  -3), 0.04, mouth))
+rayTracer.scene.append(Sphere((0, 1.1,  -3), 0.1, nose))
+
+rayTracer.scene.append(Sphere((-0.13, 1.3,  -3), 0.08, eye))
+rayTracer.scene.append(Sphere((0.13, 1.3,  -3), 0.08, eye))
+rayTracer.scene.append(Sphere((-0.11, 1.08,  -2.5), 0.03, button))
+rayTracer.scene.append(Sphere((0.11, 1.08,  -2.5), 0.03, button))
+
+# rayTracer.pointLight = PointLight(position = (-0.5, 1, 0), intensity = 1)
+# rayTracer.ambientLight = AmbientLight(strength = 0.1)
+
+rayTracer.scene.append( Sphere((-0.08, 0.9,  -3), 0.04, mouth))
+rayTracer.scene.append( Sphere((0.08, 0.9,  -3), 0.04, mouth))
+rayTracer.scene.append( Sphere((-0.20, 0.99,  -3), 0.04, mouth))
+rayTracer.scene.append( Sphere((0.20, 0.99,  -3), 0.04, mouth))
 
  
 rayTracer.rtRender()
--- sphere.py
+++ sphere.py
@@ -4,14 +4,29 @@ from mathLib import *
 
 WHITE = color(1,1,1)
 
+class AmbientLight(object):
+    def __init__(self, strength = 0, _color = WHITE):
+        self.strength = strength
+        self.color = _color
+
+class PointLight(object):
+    def __init__(self, position = (0,0,0), _color = WHITE, intensity = 1):
+        self.position = position
+        self.intensity = intensity
+        self.color = _color
+
 class Material(object):
-    def __init__(self, diffuse = WHITE):
+    def __init__(self, diffuse = WHITE, spec = 0):
         self.diffuse = diffuse
+        self.spec = spec
 
 
 class Intersect(object):
-    def __init__(self, distance):
+    def __init__(self, distance, point, normal, sceneObject):
         self.distance = distance
+        self.point = point
+        self.normal = normal
+        self.sceneObject = sceneObject
 
 
 class Sphere(object):
@@ -22,14 +37,13 @@ class Sphere(object):
 
     def ray_intersect(self, orig, dir):
         L = subVectors(self.center, orig)
-        # vector de origen al punto perpendicular del centro
         tca = dotVectors(L, dir)
         l = frobeniusNorm(L) 
         d = (l**2 - tca**2) ** 0.5
         if d > self.radius:
             return None
 
-        # Distancia de P1 al punto perpendicular del centro
+
         thc = (self.radius ** 2 - d**2) ** 0.5
         t0 = tca - thc
         t1 = tca + thc
@@ -39,4 +53,12 @@ class Sphere(object):
         if t0 < 0: 
             return None
 
-        return Intersect(distance = t0)
+
+        hit = sumVectors(orig, multiply(t0, dir))
+        norm = subVectors( hit, self.center )
+        norm = norm / frobeniusNorm(norm)
+
+        return Intersect(distance = t0,
+                         point = hit,
+                         normal = norm,
+                         sceneObject = self)
